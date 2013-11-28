e2dc9a931f74f787e928237a00880c7fc415bee1
PashaPasta <PashaPasta@users.noreply.github.com>
2013-11-28 17:19:47 -0500

    Update KaliConfigAndUpdate.sh
    Reworked Q/A sequence.  Improved feedback at end of script.

--- KaliConfigAndUpdate.sh
+++ KaliConfigAndUpdate.sh
@@ -1,87 +1,104 @@
-	
+#!/bin/bash
 
-    #!/bin/bash
-    # This script is intended for those who are new to Kali Linux and may not know what they need to do in terms of getting
-    # Kali set up and configured for basic uses.  Contact thejesterrace87@gmail.com with bug info.
-    
+# Kali Configuration and Updater version 2.0
+# This script is intended for use in new Kali Linux Installations
+# Please contact thejesterrace87@gmail.com with bugs or feature requests
+
+printf "
+
+			#############################
+			# KaliUpdater and Config v2 #
+			#############################
+
+	##############################################################
+	# Welcome, you will be presented with a few questions, please#
+	#          answer [y/n] according to your needs.             #
+	##############################################################\n\n"
+
+
+# Ask Initial questions
+
+read -p "Do you want to add the Bleeding Edge repo for more regular updates? [y/n] " answerRepo
+read -p "Do you want to install updates to Kali Linux now? [y/n] " answerUpdate
+read -p "Do you want the metasploit and postgresql services to start on boot? (Recommended) [y/n] "
+read -p "Do you want to setup OpenVAS? (Note: You will be prompted to enter a password for the OpenVAS admin user, this process may take up to an hour) [y/n] " answerOpenVAS
+read -p "Do you want to install and setup TOR with Privoxy? [y/n] " answerTOR
+read -p "Do you want to update Nikto's definitions? [y/n] " answerNikto
+
+# Logic for update and configuration steps
+
  
-    read -p "Do you want to add the Bleeding Edge repo to your sources? [y/n]" answerRepo
+
+if [[ $answerRepo = y ]] ; then
+
+        echo deb http://repo.kali.org/kali kali-bleeding-edge main >> /etc/apt/sources.list
+fi
+
+if [[ $answerUpdate = y ]] ; then
+        
+	printf "Updating Kali Linux, this stage may take about an hour to complete...\n"
+        sleep 3
+	apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade
+fi
+
+if [[ $answerServices = y ]] ; then        
+
+        service postgresql start
+    	service metasploit start
+
+	update-rc.d postgresql enable
+        update-rc.d metasploit enable
+fi
+
+if [[ $answerOpenVAS = y ]] ; then
+
+        echo ...Starting OpenVAS setup...Please be ready to enter desired OpenVAS admin password
     
-    if [[ $answerRepo = y  ]] ; then
-    	echo deb http://repo.kali.org/kali kali-bleeding-edge main >> /etc/apt/sources.list
-    fi
-    
-    read -p "Do you want to update Kali Linux now? WARNING This may take up to an hour to complete... [y/n]" answerUpdate
-    
-    if [[ $answerUpdate = y ]] ; then
-    	
-	print "Hope you have a something to kill time for a while..."
-    		apt-get update  && apt-get -y upgrade && apt-get -y dist-upgrade
-    fi 
-    
-    echo Starting postgresql and metasploit services...
+                openvas-setup
+        	openvas-setup --check-install > /root/Desktop/openvas-info.txt
+     		openvas-nvt-sync
+                openvas-feed-update
+                 
+fi
 
-    service postgresql start
-    service metasploit start
+if [[ $answerTOR = y ]] ; then
 
-    read -p "Do you want postgresql and metasploit to start on boot? [y/n] " answerServices
-    
-    if [[ $answerServices = y ]] ; then
-    	echo ...Updating rc.d...
+            apt-get install tor privoxy vidalia polipo
 
-    	update-rc.d postgresql enable
-    	update-rc.d metasploit enable
-    fi
+            echo forward-socks4a / 127.0.0.1:9050 >> /etc/privoxy/config
+            echo listen-address 127.0.0.1:8118 >> /etc/privoxy/config
+fi
+	
+if [[ $answerNikto = y ]] ; then
 
-    read -p "Would you like to setup OpenVAS now? [y/n] " answerOpenVAS
+	nikto -update
+fi
 
-    if [[ $answerOpenVAS = y ]] ; then
+# If OpenVAS was installed, check for error file, if present, print alert
 
-    	echo ...Starting OpenVAS setup...
-    
-		openvas-setup
+function filecheck () {
+	file="/root/Desktop/openvas-info.txt"
 
-    	echo ...Now starting setup diagnostics, results will be saved to /Desktop/openvas-info.txt
-      
-     sleep 10
-    	
-	echo ...May this stage may appear to be doing nothing, be patient...
-    		
-		openvas-setup --check-install > /home/root/Desktop/openvas-info.txt
-    	
-	echo ...Updating OpenVAS Definitions
-    		
-		openvas-nvt-sync
-    		openvas-feed-update
-    	
-	print "...Note: default openVAS user name is admin NOT ROOT"
-    
-	echo ...OpenVAS setup should be complete, check openvas-info.txt for errors
-    fi
+	if [ -f "$file" ] ; then
+		printf "Check /root/Desktop/openvas-info.txt for errors and recommendations"
+	fi
+}	
+if [[ $answerOpenVAS = y ]] ; then
 
-    sleep 2
+file="/root/Desktop/openvas-info.txt"
 
-    read -p "Would you like to install tor? [y/n] " answerTOR
+	filecheck
+	printf "Note: OpenVAS user name is [admin] "
+	sleep 3
+fi
 
-    if [[ $answerTOR = y ]] ; then
-
-    		apt-get install tor privoxy vidalia polipo
-
-    	echo Configuring the tor proxy for you...
-    	echo forward-socks4a / 127.0.0.1:9050 >> /etc/privoxy/config
-    	echo listen-address       127.0.0.1:8118 >> /etc/privoxy/config
-    fi
-    
-    read -p "Do you want to update Nikto's definitions? [y/n] " answerNikto
-    if [[ $answerNikto = y ]] ; then
-
-        nikto -update
-    fi
-   
+if [[ $answerTOR = y ]] ; then
+	printf "TOR has been configured with Privoxy, set your browser to use a socks4 proxy on localhost:8118"
+	sleep 3
+fi
 
 function pause () {
-	read -p "$*"
+        read -p "$*"
 }
-
     
-   pause 'Press [Enter] key to finish script...'
+pause 'Press [Enter] key to finish script...'
