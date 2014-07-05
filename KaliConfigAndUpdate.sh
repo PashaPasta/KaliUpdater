a7fcb6b353555f5ffb587ed81be7d4f796cba6c3
PashaPasta <PashaPasta@users.noreply.github.com>
2014-07-05 14:58:54 -0400

    Update KaliConfigAndUpdate.sh
    Added VPN installation option, Added additional tool installations, Added gnome-tweak-tool

--- KaliConfigAndUpdate.sh
+++ KaliConfigAndUpdate.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Kali Configuration and Updater version 2.0
+# Kali Configuration and Updater version 3.0
 # This script is intended for use in new Kali Linux Installations
 # Please contact thejesterrace87@gmail.com with bugs or feature requests
+
+
+# Changelog:
+# 7/2/2014: Added VPN installation option, Added additional tool installations, Added gnome-tweak-tool
 
 printf "
 
@@ -21,19 +25,20 @@ printf "
 function questions() {
 read -p "Do you want to add the Bleeding Edge repo for more regular updates? [y/n] " answerRepo
 read -p "Do you want to install updates to Kali Linux now? [y/n] " answerUpdate
-read -p "Do you want the metasploit and postgresql services to start on boot? (Recommended) [y/n] "
+read -p "Do you want to install packages required for VPN functionality? [y/n] " answerVPN
+read -p "Do you want the metasploit and postgresql services to start on boot? (Recommended) [y/n] " answerServices
 read -p "Do you want to setup OpenVAS? (Note: You will be prompted to enter a password for the OpenVAS admin user, this process may take up to an hour) [y/n] " answerOpenVAS
 read -p "Do you want to install and setup TOR with Privoxy? [y/n] " answerTOR
 read -p "Do you want to update Nikto's definitions? [y/n] " answerNikto
-read -p "Do you want to download additional useful scripts for pentesting purposes? [y/n]" answerScripts
+read -p "Do you want to download additional tools and scripts for pentesting purposes? [y/n]" answerScripts
 }
 
-#Flags!!!! 
+#Flags!!!!
 # If script run with -a flag, all options will automatically default to yes
 # IF script run with -h flag, README.md will be displayed
 
 if [[ $1 = -a ]] ; then
-	
+
 	read -p "Are you sure you want to install all packages and configure everything by default? [y/n] " answerWarning
 	if [[ $answerWarning = y ]] ; then
 		answerRepo=y
@@ -41,17 +46,17 @@ if [[ $1 = -a ]] ; then
 		answerOpenVAS=y
 		answerTOR=y
 		answerNikto=y
-	else 
+	else
 		printf "Verify would you do an do not want done...."
 		sleep 2
 		questions
 	fi
-	
+
 elif [[ $1 = -h ]] ; then
 
 	cat README.md
 	exit
-else 
+else
 
 	questions
 
@@ -61,7 +66,7 @@ fi
 
 # Logic for update and configuration steps
 
- 
+
 
 if [[ $answerRepo = y ]] ; then
 
@@ -69,13 +74,24 @@ if [[ $answerRepo = y ]] ; then
 fi
 
 if [[ $answerUpdate = y ]] ; then
-        
+
 	printf "Updating Kali Linux, this stage may take about an hour to complete...Hope you have some time to burn...
 	"
 	apt-get update -qq && apt-get -y upgrade -qq && apt-get -y dist-upgrade -qq
+  apt-get install unrar unace rar p7zip zip unzip p7zip-full p7zip-rar file-roller -y
+  apt-get install flashplugin-nonfree
+  update-flashplugin-nonfree --install
+  apt-get install htop nethogs
 fi
 
-if [[ $answerServices = y ]] ; then        
+# Add VPN Installation step here
+if [[ $answerVPN = y ]] ; then
+
+  apt-get install network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-strongswan network-manager-vpnc network-manager-vpnc-gnome
+
+fi
+
+if [[ $answerServices = y ]] ; then
 
         service postgresql start
     	service metasploit start
@@ -87,13 +103,13 @@ fi
 if [[ $answerOpenVAS = y ]] ; then
 
         echo ...Starting OpenVAS setup...Please be ready to enter desired OpenVAS admin password
-    
+
                 apt-get -y install nsis rpm
                 openvas-setup
         	openvas-setup --check-install > /var/log/KaliUpdater.log
      		openvas-nvt-sync
                 openvas-feed-update
-                 
+
 fi
 
 if [[ $answerTOR = y ]] ; then
@@ -103,7 +119,7 @@ if [[ $answerTOR = y ]] ; then
             echo forward-socks4a / 127.0.0.1:9050 >> /etc/privoxy/config
             echo listen-address 127.0.0.1:8118 >> /etc/privoxy/config
 fi
-	
+
 if [[ $answerNikto = y ]] ; then
 
 	nikto -update
@@ -118,13 +134,13 @@ function filecheck () {
 		printf "Check /var/log/KaliUpdater.log for errors and recommendations
 		"
 	fi
-}	
+}
 if [[ $answerOpenVAS = y ]] ; then
 
 file="/var/log/KaliUpdater.log"
 
 	filecheck
-	printf "Note: OpenVAS user name is [admin] 
+	printf "Note: OpenVAS user name is [admin]
 	"
 	sleep 3
 fi
@@ -144,13 +160,13 @@ if [[ $answerScripts = y ]] ; then
 	apt-get install python-pygraphviz
 	python rawr.py --check-install
 	#Add other tools here
-	
+
 fi
 
 function pause () {
         read -p "$*"
 }
-    
+
 pause '
 	Press [Enter] key to exit...
 	'
