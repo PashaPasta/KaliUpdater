65d321f6d3551ed65872df839f43571b4b96fc7c
PashaPasta <PashaPasta@users.noreply.github.com>
2014-07-27 20:38:47 -0400

    Update KaliConfigAndUpdate.sh
    7/27/2014: Added git and svn protocol updaters for existing git clones and svn checkouts; fixed a few formatting issues

--- KaliConfigAndUpdate.sh
+++ KaliConfigAndUpdate.sh
@@ -8,16 +8,17 @@
 # Changelog:
 # 7/2/2014: Added VPN installation option, Added additional tool installations, Added gnome-tweak-tool
 # 7/18/2014: Fixed rawr.py install, rawr devs changed from --check-install to -U
+# 7/27/2014: Added git and svn protocol updaters for existing git clones and svn checkouts; fixed a few formatting issues 
 printf "
 
-			###############################
-			# KaliUpdater and Config v3.0 #
-			###############################
+###############################
+# KaliUpdater and Config v3.0 #
+###############################
 
-	##############################################################
-	# Welcome, you will be presented with a few questions, please#
-	#          answer [y/n] according to your needs.             #
-	##############################################################\n\n"
+##############################################################
+# Welcome, you will be presented with a few questions, please#
+# answer [y/n] according to your needs. #
+##############################################################\n\n"
 
 
 
@@ -31,6 +32,7 @@ read -p "Do you want to setup OpenVAS? (Note: You will be prompted to enter a pa
 read -p "Do you want to install and setup TOR with Privoxy? [y/n] " answerTOR
 read -p "Do you want to update Nikto's definitions? [y/n] " answerNikto
 read -p "Do you want to download additional tools and scripts for pentesting purposes? [y/n]" answerScripts
+read -p "Do you want to find and install updates to all svn and git tools previously installed? [y/n]" answerSVN
 }
 
 #Flags!!!!
@@ -39,26 +41,30 @@ read -p "Do you want to download additional tools and scripts for pentesting pur
 
 if [[ $1 = -a ]] ; then
 
-	read -p "Are you sure you want to install all packages and configure everything by default? [y/n] " answerWarning
-	if [[ $answerWarning = y ]] ; then
-		answerRepo=y
-		answerUpdate=y
-		answerOpenVAS=y
-		answerTOR=y
-		answerNikto=y
-	else
-		printf "Verify would you do an do not want done...."
-		sleep 2
-		questions
-	fi
+read -p "Are you sure you want to install all packages and configure everything by default? [y/n] " answerWarning
+if [[ $answerWarning = y ]] ; then
+answerRepo=y
+answerUpdate=y
+answerVPN=y
+answerServices=y
+answerOpenVAS=y
+answerTOR=y
+answerNikto=y
+answerScripts=y
+answerSVN=y
+else
+printf "Verify would you do an do not want done...."
+sleep 2
+questions
+fi
 
 elif [[ $1 = -h ]] ; then
 
-	cat README.md
-	exit
+cat README.md
+exit
 else
 
-	questions
+questions
 
 fi
 
@@ -75,13 +81,13 @@ fi
 
 if [[ $answerUpdate = y ]] ; then
 
-	printf "Updating Kali Linux, this stage may take about an hour to complete...Hope you have some time to burn...
-	"
-	apt-get update -qq && apt-get -y upgrade -qq && apt-get -y dist-upgrade -qq
+printf "Updating Kali Linux, this stage may take about an hour to complete...Hope you have some time to burn...
+"
+apt-get update -qq && apt-get -y upgrade -qq && apt-get -y dist-upgrade -qq
   apt-get install unrar unace rar p7zip zip unzip p7zip-full p7zip-rar file-roller -y
-  apt-get install flashplugin-nonfree -y 
+  apt-get install flashplugin-nonfree -y
   update-flashplugin-nonfree --install
-  apt-get install htop nethogs gnome-tweak-tool -y 
+  apt-get install htop nethogs gnome-tweak-tool -y
 fi
 
 # Add VPN Installation step here
@@ -93,11 +99,11 @@ fi
 
 if [[ $answerServices = y ]] ; then
 
-        service postgresql start
-    	service metasploit start
+  service postgresql start
+  service metasploit start
 
-	update-rc.d postgresql enable
-        update-rc.d metasploit enable
+  update-rc.d postgresql enable
+  update-rc.d metasploit enable
 fi
 
 if [[ $answerOpenVAS = y ]] ; then
@@ -106,8 +112,8 @@ if [[ $answerOpenVAS = y ]] ; then
 
                 apt-get -y install nsis rpm
                 openvas-setup
-        	openvas-setup --check-install > /var/log/KaliUpdater.log
-     		openvas-nvt-sync
+                openvas-setup --check-install > /var/log/KaliUpdater.log
+                openvas-nvt-sync
                 openvas-feed-update
 
 fi
@@ -117,51 +123,70 @@ if [[ $answerTOR = y ]] ; then
             apt-get -y -qq install tor privoxy vidalia polipo
 
             echo forward-socks4a / 127.0.0.1:9050 . >> /etc/privoxy/config
-            echo forward-socks4  / 127.0.0.1:9050 . >> /etc/privoxy/config
-            echo forward-socks5  / 127.0.0.1:9050 . >> /etc/privoxy/config
+            echo forward-socks4 / 127.0.0.1:9050 . >> /etc/privoxy/config
+            echo forward-socks5 / 127.0.0.1:9050 . >> /etc/privoxy/config
             echo listen-address 127.0.0.1:8118 >> /etc/privoxy/config
 fi
 
 if [[ $answerNikto = y ]] ; then
 
-	nikto -update
+nikto -update
 fi
 
 # If OpenVAS was installed, check for error file, if present, print alert
 
 function filecheck () {
-	file="/var/log/KaliUpdater.log"
+file="/var/log/KaliUpdater.log"
 
-	if [ -f "$file" ] ; then
-		printf "Check /var/log/KaliUpdater.log for errors and recommendations
-		"
-	fi
+if [ -f "$file" ] ; then
+printf "Check /var/log/KaliUpdater.log for errors and recommendations
+"
+fi
 }
 if [[ $answerOpenVAS = y ]] ; then
 
 file="/var/log/KaliUpdater.log"
 
-	filecheck
-	printf "Note: OpenVAS user name is [admin]
-	"
-	sleep 3
+filecheck
+printf "Note: OpenVAS user name is [admin]
+"
+sleep 3
 fi
 
 if [[ $answerTOR = y ]] ; then
-	printf "TOR has been configured with Privoxy, set your browser to use a socks4 proxy on localhost:8118
-	"
-	sleep 3
+printf "TOR has been configured with Privoxy, set your browser to use a socks4 proxy on localhost:8118
+"
+sleep 3
 fi
 
 if [[ $answerScripts = y ]] ; then
-	printf "Downloading scripts...."
-	mkdir /root/scripts
-	cd /root/scripts
-	git clone https://bitbucket.org/al14s/rawr.git
-	cd rawr
-	apt-get install python-pygraphviz
-	python rawr.py -U
-	#Add other tools here
+printf "Downloading scripts...."
+mkdir /root/scripts
+cd /root/scripts
+git clone https://bitbucket.org/al14s/rawr.git
+cd rawr
+apt-get install python-pygraphviz
+python rawr.py -U
+#Add other tools here
+
+fi
+
+if [[ $answerSVN = y ]] ; then
+  # Start git updater
+  find . -name .git -type d -prune > /tmp/gitsfound.txt
+  rev /tmp/gitsfound.txt | cut -c 5- | rev > /tmp/gitsfound-fixed.txt
+  cat /tmp/gitsfound-fixed.txt | cut -c 3- | grep '[a-z]' > /tmp/gitsfound-fixed2.txt
+  for i in `cat /tmp/gitsfound-fixed2.txt`; do cd /root/$i && git pull && cd;done
+  rm /tmp/gitsfound*
+  # Finish git updater
+
+  # Start svn updater
+  find . -name .svn -type d -prune > /tmp/svnsfound.txt
+  rev /tmp/svnsfound.txt | cut -c 5- | rev > /tmp/svnsfound-fixed.txt
+  cat /tmp/svnsfound-fixed.txt | cut -c 3- | grep '[a-z]' > /tmp/svnsfound-fixed2.txt
+  for i in `cat /tmp/svnsfound-fixed2.txt` ; do cd /root/$i && svn update && cd;done
+  rm /tmp/svnsfound*
+  # Finish svn updater
 
 fi
 
@@ -170,5 +195,5 @@ function pause () {
 }
 
 pause '
-	Press [Enter] key to exit...
-	'
+Press [Enter] key to exit...
+'
